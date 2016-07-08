552de7cd8a6eba21b23e3ed6ca2d669ac1b413fa
Rovsh <ravshan.sharipov@gmail.com>
2016-07-09 00:10:47 +1000

    Add files via upload

---
+++ Q1.erl
@@ -0,0 +1,18 @@
+-module(q1).
+-export([sum_dev3or5/2, start/1]).
+
+start(X) ->
+	sum_dev3or5(X-1,0).
+
+sum_dev3or5(0,R) ->
+	io:format("~p~n", [R]),
+    done;
+sum_dev3or5(X,R) ->
+if
+	X rem 3 == 0 ->
+		Y=X;
+	X rem 5 == 0 ->
+		Y=X;
+	true -> Y = 0
+end,
+sum_dev3or5(X-1,R+Y).
