1e62766fdc9c5f742afc4232db179676d6de4202
Rovsh <ravshan.sharipov@gmail.com>
2016-07-21 14:14:25 +1000

    Logic has corrected
    (messenger@RAVSHAN)4> q4:start(3).
    max_palindrome = 906609
    ok

--- Q4.erl
+++ Q4.erl
@@ -1,22 +1,24 @@
 %4. A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 % 	Find the largest palindrome made from the product of two 3-digit numbers.
--module(q4).
--export([is_palindrome/2, start/1]).
+-module(q4_1).
+-export([max_palindrome/4, start/1]).
 
 start(DigitQuantity) ->
 	S = list_to_integer(lists:duplicate(DigitQuantity,57)),
-	is_palindrome(S,S).
+	io:format("max_palindrome = ~p~n", [max_palindrome(0,0,S,0)]).
 
-is_palindrome(X,Y) ->
+max_palindrome(X, Y, S, MaxValue) ->
 	NormalList = integer_to_list(X*Y), 
 	ReverseList = lists:reverse(integer_to_list(X*Y)),
-%	io:format("current_value -> ~p~n", [NormalList]),
+%	io:format("~p * ~p = ~p~n", [X, Y, NormalList]),
 	if
-		NormalList == ReverseList	->
-			io:format("(~p*~p = largest_palindrome -> ~p~n", [X,Y,NormalList]),
-			exit;
-		X >= Y -> 
-			is_palindrome(X-1,Y);
-		X < Y -> 
-			is_palindrome(X,Y-1)
+		Y > S -> 
+			max_palindrome(X+1, 0, S, MaxValue);
+		X > S -> 
+			MaxValue;
+		(NormalList == ReverseList) andalso (X*Y > MaxValue) ->
+%			io:format("(~p*~p = largest_palindrome -> ~p~n", [X, Y, MaxValue]),
+			max_palindrome(X, Y+1, S, X*Y);
+		true -> 
+			max_palindrome(X, Y+1, S, MaxValue)
 	end.
